d9ee07a18524bab415e14f477a99088ac314f210
Maeda, Kazuya <kazuya.maeda@gmail.com>
2019-05-01 16:05:19 +0900

    fix

--- make_sql.py
+++ make_sql.py
@@ -1,39 +1,32 @@
-#! /anaconda3/bin/python
 import sys
 import re
 
-#print ('Hello')
-if (len(sys.argv) > 1): 
-    print('# arg: ' + sys.argv[1])
-
-draft = open("draft.txt", "r")
-out = open("InsertQuote.ddl", "w")
-
+workId = 0
 phrase = ''
 url = ''
 sql = ''
 
-# 一行ずつ読み込んでは表示する
-#for line in draft:
+if (len(sys.argv) > 1):
+    workId = sys.argv[1]
+    print('# work_id: ' + workId)
+else:
+    print('# Please put work_id as argument')
+
+draft = open("draft.txt", "r")
+out = open("InsertQuote.ddl", "w")
+
 for line in draft.read().splitlines():
     if 'http' in line:
         url = line
-#        url = line.rstrip('\n')
     else:
         phrase = line
-#        phrase = line.rstrip('\n')
         continue
 
-    sql = 'INSERT INTO quote(phrase_org, speaker, category_id, work_id, image_url) values(\'' + phrase + '\', \'\', , 100, \'' + url + '\');\n'
+    # Format: INSERT INTO quote(phrase_org, speaker, category_id, work_id, image_url) values('', '', , workId, '');
+    sql = 'INSERT INTO quote(phrase_org, speaker, category_id, work_id, image_url) values(\'' + phrase + '\', \'\', , ' + workId + ', \'' + url + '\');\n'
 
     out.write(sql)
     
-
-#out.write(sql)    
-
-# ファイルをクローズする
 draft.close()
 out.close()
 
-
-# INSERT INTO quote(phrase_org, speaker, category_id, work_id, image_url) values('', '', , 100, '');
